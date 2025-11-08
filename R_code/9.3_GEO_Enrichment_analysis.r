a4b26348c10cf685e119395f9d6e41969ce1f3fd
ABenMao <doc_cheng@yeah.net>
2025-11-08 18:00:34 +0800

    Update 9.3_GEO_Enrichment_analysis.r

--- R_code/9.3_GEO_Enrichment_analysis.r
+++ R_code/9.3_GEO_Enrichment_analysis.r
@@ -34,8 +34,8 @@ genelist <- bitr(DEG$Gene, fromType="SYMBOL",
                  toType="ENTREZID", OrgDb='org.Hs.eg.db')
 DEG <- inner_join(DEG,genelist,by=c("Gene"="SYMBOL"))
 
-# GO分析
-ego <- enrichGO(gene = DEG$ENTREZID,
+#### GO分析 ####
+go <- enrichGO(gene = DEG$ENTREZID,
                 OrgDb = org.Hs.eg.db, 
                 ont = "all",
                 pAdjustMethod = "BH",
@@ -43,27 +43,68 @@ ego <- enrichGO(gene = DEG$ENTREZID,
                 pvalueCutoff =0.05, 
                 qvalueCutoff =0.05,
                 readable = TRUE)
-go_res <- ego@result # 提取GO分析结果
+go_res <- go@result # 提取GO分析结果
 
-# KEGG
-kk <- enrichKEGG(gene         = DEG$ENTREZID,
+# 保存GO分析结果
+save(go,go_res,file = "GO_analysis.Rdata")
+
+# 可视化（可调整Top数量）
+# 柱状图
+barplot(go, showCategory = 10,color = "pvalue")
+
+# 气泡图
+dotplot(go, showCategory = 10)
+
+# 分类展示
+barplot(go, drop = TRUE, showCategory =10,split="ONTOLOGY") + 
+  facet_grid(ONTOLOGY~., scale='free')
+dotplot(go,showCategory = 10,split="ONTOLOGY") + 
+  facet_grid(ONTOLOGY~., scale='free')
+
+#### KEGG ####
+kegg <- enrichKEGG(gene         = DEG$ENTREZID,
                  organism     = 'hsa',
                  pvalueCutoff = 0.1,
                  qvalueCutoff =0.1)
-kegg_res <- kk@result
+kegg_res <- kegg@result
 
-# GSEA
-msigdb_GMTs <- "msigdb_v7.0_GMTs"
-msigdb <- "c5.all.v7.0.entrez.gmt"    #c2.all.v7.0.entrez.gmt 或 c5.all.v7.0.entrez.gmt
-#读取上面指定的gmt文件
-kegmt <- read.gmt(file.path(msigdb_GMTs,msigdb))
+# 保存KEGG分析结果
+save(kegg,kegg_res,file = "KEGG_analysis.Rdata")
 
-geneList = DEG[,2]
+# 可视化（可调整Top数量）
+# 柱状图
+barplot(kegg, showCategory = 20,color = "pvalue")
+# 气泡图
+dotplot(kegg, showCategory = 20)
+
+#### GSEA ####
+# 读取参考的gmt文件
+reference_gmt <- read.gmt("c5.all.v7.0.entrez.gmt")
+
+# 准备排序基因列表
+geneList = DEG[,2] # 注意一下，选择LogFC列
 names(geneList) = as.character(DEG[,'ENTREZID'])
 head(geneList)
 geneList = sort(geneList, decreasing = TRUE)
 
+# GSEA分析
 set.seed(1)
-KEGG<-GSEA(geneList,TERM2GENE = kegmt) #GSEA分析
+gsea<-GSEA(geneList,TERM2GENE = reference_gmt)
+
+# 结果保存
+gsea_result_df <- as.data.frame(gsea)
+write.table(gsea_result_df,file="GSEA_MSigDb_C5_result.txt",sep = "\t",row.names = T,col.names = NA,quote = F)
+save(gsea,gsea_result_df,file = "GSEA_deg_SPP1.rda")
+
+# 可视化
+# 单通路绘制
+library(enrichplot)
+gseaplot2(gsea,1,color="red")
+gseaplot2(gsea,3,color="red",pvalue_table = T)
+
+# 多通路绘制（第一个参数为通路序号，第二个参数为图像包含部分）
+gseaplot2(gsea, geneSetID = c(1,3), subplots = 1:3)
+gseaplot2(gsea, geneSetID = 1:3, subplots = 1:2)
+gseaplot2(gsea, geneSetID = 1:10, subplots = 1:3)
 
 # 等等，与TCGA的分析大致相同
